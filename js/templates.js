cb470277de5fd2d01f3dae829f1005d102b0e480
Sean van Dijk <seandijk@gmail.com>
2019-09-30 21:20:12 +0200

    Added icons that cycle on click using radoibuttons and labels. They get loaded with a Html template tag and JavaScript.

---
+++ js/templates.js
@@ -0,0 +1,54 @@
+var images = [
+    'images/cycle/desktop_windows-24px.svg',
+    'images/cycle/cake-24px.svg',
+    'images/cycle/airline_seat_individual_suite-24px.svg',
+    'images/cycle/directions_bike-24px.svg',
+    'images/cycle/highlight-24px.svg',
+    'images/cycle/sentiment_satisfied_alt-24px.svg',
+    'images/cycle/sports_esports-24px.svg',
+    'images/cycle/sports_hockey-24px.svg',
+    'images/cycle/videogame_asset-24px.svg',
+]
+
+function cycleImageList(groupname, imageList, location) {
+    var template = document.querySelector('#template-cycle-img');
+    console.log(template)
+    for (let i = 0; i < imageList.length; i++) {
+        const element = imageList[i];
+        var clone = document.importNode(template.content, true);
+
+        var input = clone.querySelector("input");
+        var label = clone.querySelector("label");
+        var img = clone.querySelector("img");
+
+        console.log(input)
+
+        input.id = groupname + "-img-" + i
+        input.name = groupname
+
+        if (i == 0) {
+            // The first input should be checked, to begin at the start.
+            input.checked = true;
+        }
+        if (i == imageList.length - 1) {
+            // If it is the last element, we want to link it to the first. 
+            label.htmlFor = groupname + "-img-" + 0
+        } else {
+            // Otherwise just link it to the next one.
+            label.htmlFor = groupname + "-img-" + (i + 1)
+        }
+        img.src = element
+            // console.log(input)
+        console.log(input)
+        location.appendChild(clone)
+
+        // Remove placeholders.
+        location.querySelectorAll('.placeholder').forEach(element => {
+            location.removeChild(element)
+        });
+    }
+}
+window.onload = function() {
+    // Do the work after everything was loaded (DOM, media elements)
+    cycleImageList("icon-cycle", images, document.querySelector("#cycle-img-wrapper"))
+}
